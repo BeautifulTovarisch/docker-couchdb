0c5603e0ffc67ab33a0fdcaf26a39f01a62f91b7
Anthony <re@towson.edu>
2018-04-08 03:12:37 -0400

    successful connection established over docker bridge network

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const methodOverride = require( 'method-override' );
 const PORT = process.env.PORT || 2305;
 const HOST = process.env.HOST || '0.0.0.0';
 
+// The connection string is comprised of the following:
+// @database		- name of service in docker-compose.yml
+// COUCHDB_USER		- admin user
+// DATABASE_PORT	- port on which database service is running
+// COUCHDB_PASSWORD	- admin password
+
+const connection = `http://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASSWORD}@database:${process.env.DATABASE_PORT}`;
+
+const nano = require( 'nano' )( connection );
+
 /////////////// MiddleWares ///////////////
 
 app.use( morgan( 'dev' ) );
@@ -19,7 +29,7 @@ app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { "extended": true } ) );
 app.use( express.static( __dirname ) );
 
-/////////////// Routes ///////////////
+/////////////// Routes ///////////////////
 
 const index = require( path.resolve( 'server/index/router' ) );
 
